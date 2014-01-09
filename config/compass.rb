a9453c46017ca60a22305fe03059b46bd02ac782
Alex Fogg <afogg@canvas.is>
2014-01-09 10:56:29 -0500

    changed relative/absolute asset compilation

--- config/compass.rb
+++ config/compass.rb
@@ -7,9 +7,10 @@ css_dir = "original_assets/stylesheets"
 sass_dir = "original_assets/stylesheets"
 images_dir = "original_assets/images"
 javascripts_dir = "original_assets/javascripts"
+http_generated_images_path = "/images/"
 generated_images_dir = "compiled_assets/images"
 
 # Options
 sass_options = {:cache => false}
 output_style = :expanded
-relative_assets = true
+relative_assets = false
